f4572556f879be895e6080e558df343280209bd3
Tatariuinpalafoz@gmail.com <rogeriopalafoz@gmail.com>
2022-12-16 15:25:17 -0300

    Versão/16-12-22.Potenciação

--- Codigo/TopicosCalculadora/src/topicoscalculadora/Operacoes.java
+++ Codigo/TopicosCalculadora/src/topicoscalculadora/Operacoes.java
@@ -1,7 +1,14 @@
 package topicoscalculadora;
 public class Operacoes {
 
-
+	public int potencia(int a, int b){
+		//Rogerio Silva Palafoz Junior
+	   int c = 1;
+       for(int i = 0;i < b;i++){
+           c *= a;
+       }
+        return c;
+    }
 
     
 }
